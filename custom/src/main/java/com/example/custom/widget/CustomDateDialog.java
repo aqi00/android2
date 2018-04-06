2a8fe8b86e4850fc52b781e74f712bab80de619c
aqi00 <aqi00@163.com>
2018-04-06 13:27:41 +0800

    第6章 自定义控件

--- custom/src/main/java/com/example/custom/widget/CustomDateDialog.java
+++ custom/src/main/java/com/example/custom/widget/CustomDateDialog.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.DatePicker;
-import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.TextView;
 
 public class CustomDateDialog implements OnClickListener {
