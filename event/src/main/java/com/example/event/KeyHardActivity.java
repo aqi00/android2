85ea80d12687b72c9a1d3528ab1590b1f4151f71
aqi00 <aqi00@163.com>
2018-05-08 17:24:05 +0800

    第11章 事件

--- event/src/main/java/com/example/event/KeyHardActivity.java
+++ event/src/main/java/com/example/event/KeyHardActivity.java
@@ -1,11 +1,18 @@
 package com.example.event;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.widget.TextView;
+
+import com.example.event.util.DateUtil;
 
 /**
  * Created by ouyangshen on 2017/11/23.
@@ -20,6 +27,7 @@ public class KeyHardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key_hard);
         tv_result = findViewById(R.id.tv_result);
+        initDesktopRecevier(); // 初始化桌面广播
     }
 
     // 在发生物理按键动作时触发
@@ -50,4 +58,44 @@ public class KeyHardActivity extends AppCompatActivity {
         }
     };
 
+    // 初始化桌面广播。用于监听按下主页键和任务键
+    private void initDesktopRecevier() {
+        // 创建一个返回桌面的广播接收器
+        mDesktopRecevier = new DesktopRecevier();
+        // 创建一个意图过滤器，只接收关闭系统对话框（即返回桌面）的广播
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        // 给当前页面注册广播接收器
+        registerReceiver(mDesktopRecevier, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 注销当前页面的广播接收器
+        unregisterReceiver(mDesktopRecevier);
+    }
+
+    private DesktopRecevier mDesktopRecevier; // 声明一个返回桌面的广播接收器对象
+    // 定义一个返回到桌面的广播接收器
+    class DesktopRecevier extends BroadcastReceiver {
+        private final String SYSTEM_DIALOG_REASON_KEY = "reason"; // 键名
+        private final String SYSTEM_DIALOG_REASON_HOME = "homekey"; // 主页键
+        private final String SYSTEM_DIALOG_REASON_TASK = "recentapps"; // 任务键
+
+        // 在收到返回桌面广播时触发
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
+                String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
+                if (!TextUtils.isEmpty(reason)) {
+                    if (reason.equals(SYSTEM_DIALOG_REASON_HOME)) { // 按下了主页键
+                        desc = String.format("%s%s\t 按键为主页键\n", desc, DateUtil.getNowTime());
+                        tv_result.setText(desc);
+                    } else if (reason.equals(SYSTEM_DIALOG_REASON_TASK)) { // 按下了任务键
+                        desc = String.format("%s%s\t 按键为任务键\n", desc, DateUtil.getNowTime());
+                        tv_result.setText(desc);
+                    }
+                }
+            }
+        }
+    }
 }
