6a76ddfbc00257c8368b46c17687869e4bfa7d9e
aqi00 <aqi00@163.com>
2018-05-13 17:53:29 +0800

    第3章 中级控件

--- middle/src/main/java/com/example/middle/ActUriActivity.java
+++ middle/src/main/java/com/example/middle/ActUriActivity.java
@@ -30,18 +30,21 @@ public class ActUriActivity extends AppCompatActivity implements OnClickListener
         // 获取编辑框的输入文本
         String phone = et_phone.getText().toString();
         if (v.getId() == R.id.btn_call) { // 点击了直接拨号按钮
+            // 拨号功能还需在AndroidManifest.xml中添加拨号权限配置
             Intent intent = new Intent(); // 创建一个新意图
             intent.setAction(Intent.ACTION_CALL); // 设置意图动作为直接拨号
             Uri uri = Uri.parse("tel:" + phone); // 声明一个拨号的Uri
             intent.setData(uri); // 设置意图前往的路径
             startActivity(intent); // 启动意图通往的活动页面
         } else if (v.getId() == R.id.btn_dial) { // 点击了准备拨号按钮
+            // 拨号功能还需在AndroidManifest.xml中添加拨号权限配置
             Intent intent = new Intent(); // 创建一个新意图
             intent.setAction(Intent.ACTION_DIAL); // 设置意图动作为准备拨号
             Uri uri = Uri.parse("tel:" + phone); // 声明一个拨号的Uri
             intent.setData(uri); // 设置意图前往的路径
             startActivity(intent); // 启动意图通往的活动页面
         } else if (v.getId() == R.id.btn_sms) { // 点击了发送短信按钮
+            // 发送短信还需在AndroidManifest.xml中添加发短信的权限配置
             Intent intent = new Intent(); // 创建一个新意图
             intent.setAction(Intent.ACTION_SENDTO); // 设置意图动作为发短信
             Uri uri = Uri.parse("smsto:" + phone); // 声明一个发送短信的Uri
