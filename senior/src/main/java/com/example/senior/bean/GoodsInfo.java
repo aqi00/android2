c25ed73362acd074c1250e3757695988c1fd7d92
aqi00 <aqi00@163.com>
2018-05-22 15:14:32 +0800

    第5章 高级控件

--- senior/src/main/java/com/example/senior/bean/GoodsInfo.java
+++ senior/src/main/java/com/example/senior/bean/GoodsInfo.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 import com.example.senior.R;
 
 public class GoodsInfo {
-    public long rowid;
-    public int xuhao;
-    public String name;
-    public String desc;
-    public float price;
-    public String thumb_path;
-    public String pic_path;
-    public int thumb;
-    public int pic;
+    public long rowid; // 行号
+    public int xuhao; // 序号
+    public String name; // 名称
+    public String desc; // 描述
+    public float price; // 价格
+    public String thumb_path; // 小图的保存路径
+    public String pic_path; // 大图的保存路径
+    public int thumb; // 小图的资源编号
+    public int pic; // 大图的资源编号
 
     public GoodsInfo() {
         rowid = 0L;
@@ -27,27 +27,33 @@ public class GoodsInfo {
         pic = 0;
     }
 
+    // 声明一个手机商品的名称数组
     private static String[] mNameArray = {
-            "iphone7", "Mate8", "小米5", "vivo X6S", "OPPO R9plus", "魅族Pro6"
+            "iPhone8", "Mate10", "小米6", "OPPO R11", "vivo X9S", "魅族Pro6S"
     };
+    // 声明一个手机商品的描述数组
     private static String[] mDescArray = {
-            "Apple iPhone 7 128GB 玫瑰金色 移动联通电信4G手机",
-            "华为 HUAWEI Mate8 3GB+32GB版 移动联通4G手机（月光银）",
-            "小米手机5 全网通 高配版 3GB内存 64GB 白色",
-            "vivo X6S 金色 全网通4G 双卡双待 4GB+64GB",
-            "OPPO R9plus 4GB+64GB内存版 金色 全网通4G手机 双卡双待",
-            "魅族Pro6全网通公开版 4+32GB 银白色 移动联通电信4G手机 双卡双待"
+            "Apple iPhone 8 256GB 玫瑰金色 移动联通电信4G手机",
+            "华为 HUAWEI Mate10 6GB+128GB 全网通（香槟金）",
+            "小米 MI6 全网通版 6GB+128GB 亮白色",
+            "OPPO R11 4G+64G 全网通4G智能手机 玫瑰金",
+            "vivo X9s 4GB+64GB 全网通4G拍照手机 玫瑰金",
+            "魅族 PRO6S 4GB+64GB 全网通公开版 星空黑 移动联通电信4G手机"
     };
-    private static float[] mPriceArray = {5888, 2499, 1799, 2298, 2499, 2199};
+    // 声明一个手机商品的价格数组
+    private static float[] mPriceArray = {6888, 3999, 2999, 2899, 2698, 2098};
+    // 声明一个手机商品的小图数组
     private static int[] mThumbArray = {
             R.drawable.iphone_s, R.drawable.huawei_s, R.drawable.xiaomi_s,
-            R.drawable.vivo_s, R.drawable.oppo_9p_s, R.drawable.meizu_s
+            R.drawable.oppo_s, R.drawable.vivo_s, R.drawable.meizu_s
     };
+    // 声明一个手机商品的大图数组
     private static int[] mPicArray = {
             R.drawable.iphone, R.drawable.huawei, R.drawable.xiaomi,
-            R.drawable.vivo, R.drawable.oppo_9p, R.drawable.meizu
+            R.drawable.oppo, R.drawable.vivo, R.drawable.meizu
     };
 
+    // 获取默认的手机信息列表
     public static ArrayList<GoodsInfo> getDefaultList() {
         ArrayList<GoodsInfo> goodsList = new ArrayList<GoodsInfo>();
         for (int i = 0; i < mNameArray.length; i++) {
