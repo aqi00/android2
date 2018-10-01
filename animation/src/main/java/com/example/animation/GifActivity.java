54a53a9414cf9a6d3a81c764942273595084fd6c
aqi00 <aqi00@163.com>
2018-10-01 12:57:30 +0800

    第12章 动画

--- animation/src/main/java/com/example/animation/GifActivity.java
+++ animation/src/main/java/com/example/animation/GifActivity.java
@@ -4,8 +4,13 @@ import java.io.InputStream;
 
 import com.example.animation.util.GifImage;
 
+import android.annotation.TargetApi;
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.Animatable;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -15,18 +20,45 @@ import android.widget.Toast;
  * Created by ouyangshen on 2017/11/27.
  */
 public class GifActivity extends AppCompatActivity {
+    private final static String TAG = "GifActivity";
+    private ImageView iv_gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
-        showGifAnimation(); // 显示GIF动画
+        // 从布局文件中获取名叫iv_gif的图像视图
+        iv_gif = findViewById(R.id.iv_gif);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            showGifAnimationNew(); // 利用Android9.0新增的AnimatedImageDrawable显示GIF动画
+        } else {
+            showGifAnimationOld(); // 显示GIF动画
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.P)
+    private void showGifAnimationNew() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            try {
+                // 利用Android9.0新增的ImageDecoder读取gif图片
+                ImageDecoder.Source source = ImageDecoder.createSource(
+                        getResources(), R.drawable.welcome);
+                // 从数据源中解码得到gif图形数据
+                Drawable gifDrawable = ImageDecoder.decodeDrawable(source);
+                // 设置图像视图的图形为gif图片
+                iv_gif.setImageDrawable(gifDrawable);
+                // 如果是动画图形，则开始播放动画
+                if (gifDrawable instanceof Animatable) {
+                    ((Animatable) iv_gif.getDrawable()).start();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     // 显示GIF动画
-    private void showGifAnimation() {
-        // 从布局文件中获取名叫iv_gif的图像视图
-        ImageView iv_gif = findViewById(R.id.iv_gif);
+    private void showGifAnimationOld() {
         // 从资源文件welcome.gif中获取输入流对象
         InputStream is = getResources().openRawResource(R.raw.welcome);
         // 创建一个GIF图像对象
