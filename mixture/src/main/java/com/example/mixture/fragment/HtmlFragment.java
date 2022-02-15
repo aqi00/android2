963d8dc2f3d2ad52b613c5cd2426156ff7d4beec
aqi00 <aqi00@163.com>
2022-02-15 16:09:54 +0800

    第14章 融合技术

--- mixture/src/main/java/com/example/mixture/fragment/HtmlFragment.java
+++ mixture/src/main/java/com/example/mixture/fragment/HtmlFragment.java
@@ -34,6 +34,10 @@ public class HtmlFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_html, container, false);
         // 从布局文件中获取名叫wv_content的网页视图
         WebView wv_content = mView.findViewById(R.id.wv_content);
+        // 设置是否允许访问文件，如WebView访问存储卡的文件。
+        // 不过assets与res文件不受此限制，仍然可以通过“file:///android_asset”和“file:///android_res”访问
+        // Android11开始该值默认为false，故Android11必须设置为true才能访问存储卡的网页
+        wv_content.getSettings().setAllowFileAccess(true);
         // 命令网页视图加载指定路径的网页
         wv_content.loadUrl("file:///" + htmlPath);
         return mView;
