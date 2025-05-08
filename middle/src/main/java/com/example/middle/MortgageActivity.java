caf4725dc68f4d7a00389c98537d1f099c942265
oys <aqi00@163.com>
2025-05-08 10:05:41 +0800

    第3章 中级控件

--- middle/src/main/java/com/example/middle/MortgageActivity.java
+++ middle/src/main/java/com/example/middle/MortgageActivity.java
@@ -106,6 +106,7 @@ public class MortgageActivity extends AppCompatActivity implements OnClickListen
     }
 
     private String[] ratioDescArray = {
+            "2025年5月8日 五年期商贷利率 3.60%　公积金利率 2.60%",
             "2024年10月21日 五年期商贷利率 3.60%　公积金利率 2.85%",
             "2024年07月22日 五年期商贷利率 3.85%　公积金利率 2.85%",
             "2024年05月18日 五年期商贷利率 3.95%　公积金利率 2.85%",
@@ -120,8 +121,8 @@ public class MortgageActivity extends AppCompatActivity implements OnClickListen
             "2014年11月22日 五年期商贷利率 6.15%　公积金利率 4.25%",
             "2012年07月06日 五年期商贷利率 6.55%　公积金利率 4.50%",
     };
-    private double[] businessArray = {3.60, 3.85, 3.95, 3.95, 4.20, 4.65, 4.90, 5.15, 5.40, 5.65, 5.90, 6.15, 6.55};
-    private double[] accumulationArray = {2.85, 2.85, 2.85, 3.10, 3.10, 3.10, 3.25, 3.25, 3.50, 3.75, 4.00, 4.25, 4.50};
+    private double[] businessArray = {3.60, 3.60, 3.85, 3.95, 3.95, 4.20, 4.65, 4.90, 5.15, 5.40, 5.65, 5.90, 6.15, 6.55};
+    private double[] accumulationArray = {2.60, 2.85, 2.85, 2.85, 3.10, 3.10, 3.10, 3.25, 3.25, 3.50, 3.75, 4.00, 4.25, 4.50};
 
     // 定义一个基准利率的选择监听器
     class RatioSelectedListener implements OnItemSelectedListener {
